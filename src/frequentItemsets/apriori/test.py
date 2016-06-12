bc3735bcd305312dac7b3a5b7cc9c31a65acf5e1
XiaoYuan [肖源] <XiaoYuan1@kingsoft.cn>
2016-06-12 21:20:41 +0800

    apriori is done

---
+++ src/frequentItemsets/apriori/test.py
@@ -0,0 +1,14 @@
+'''
+Created on 2016-6-12
+
+@author: XiaoYuan1
+'''
+
+
+import prior
+
+dataset = prior.loadDataSet()
+L,supportData = prior.apriori(dataset, 0.5)
+
+print L
+rules = prior.generateRules(L, supportData, 0.5)
