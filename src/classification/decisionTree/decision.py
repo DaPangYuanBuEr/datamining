3f4ef1b04083f5d7a05a5d7ddf2ef8ea1c7ba84b
XiaoYuan [肖源] <XiaoYuan1@kingsoft.cn>
2016-07-01 19:04:38 +0800

    add classification

---
+++ src/classification/decisionTree/decision.py
@@ -0,0 +1,33 @@
+'''
+Created on 2016-7-1
+
+@author: XiaoYuan1
+'''
+
+def createDataSet():
+    dataSet = [[1,1,'yes'],
+               [1,1,'yes'],
+               [1,0,'no'],
+               [0,1,'no'],
+               [0,1,'no']
+                ]
+    labels = ['no surfacing','flippers']
+    return dataSet,labels
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reversed = True)
+
+
+
+def createTree(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if(len(dataSet[0] == 1)):
+        return 
