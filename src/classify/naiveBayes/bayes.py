fc1bba683a5b11c3c9dc1cca1823a8359bb5433a
DaPangYuanBuEr <xiaoyuan@xiaoyuandeMacBook-Pro.local>
2016-08-20 12:17:17 +0800

    完成朴素贝叶斯

--- src/classify/naiveBayes/bayes.py
+++ src/classify/naiveBayes/bayes.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-    或者  #coding=utf-8
 '''
 Created on 2016-8-19
 http://blog.csdn.net/dream_angel_z/article/details/46120867
@@ -7,7 +8,8 @@ http://blog.csdn.net/dream_angel_z/article/details/46120867
 
 from numpy import *
 from numpy.core.numeric import ones
-from math import log
+
+
 
 '创建数据集'
 def createDataSet():
@@ -32,7 +34,7 @@ def createVocabList(dataSet):
 '统计每个样本在词库中的出现情况'
 '将单个样本映射到词库中，统计单个样本在词库中出现的情况，1，表示出现过，0，表示未出现'
 def setOfWords2Vex(vocabList,inputSet):
-    returnVec = [0] * len(inputSet)
+    returnVec = [0] * len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
@@ -64,12 +66,18 @@ def tranNBO(trainMatrix,trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = log(p1Num/p1Denom)
-    p0Vect = log(p0Num/p0Denom)
-    return p0Vect,p1Vect,pAbusive
+    p1Vect = log(p1Num/p1Denom) #计算类标签为1时，其它属性发生的条件概率
+    p0Vect = log(p0Num/p0Denom) #计算类标签为0时，其它属性发生的条件概率
+    return p0Vect,p1Vect,pAbusive #返回条件概率喝类标签为1的概率
     
     
-'训练贝叶斯分类算法' 
+'训练贝叶斯分类算法'
+'vec2Classify－带分类样本在词库中的映射集合'
+'p0Vec表示条件概率P(wi|c=0)'
+'p1Vec表示条件概率P(wi|c=1)'
+'pClass1表示类标签为1时的概率P(c=1)'
+'p1表示的是lnp(w1|c=1)p(w2|c=1)...p(wn|c=1)∗p(c=1)'
+'p0表示lnp(w1|c=0)p(w2|c=0)...p(wn|c=0)∗p(c=0)'
 def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
     p1 = sum(vec2Classify * p1Vec) + log(pClass1)
     p0 = sum(vec2Classify * p0Vec) + log(1.0-pClass1)
@@ -80,11 +88,28 @@ def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
     
 
 
-
+def testingNB():
+    #加载数据
+    listOposts,listclasses = createDataSet()
+    #常见词库
+    myVocabList = createVocabList(listOposts)
+    trainMat = []
+    for postinDoc in listOposts:
+        trainMat.append(setOfWords2Vex(myVocabList, postinDoc))
+    #计算条件概率
+    p0V,p1V,pAb = tranNBO(array(trainMat), array(listclasses))
+    
+    #测试1
+    testEntry = ['love','my','dalmation']
+    thisDoc = array(setOfWords2Vex(myVocabList, testEntry))
+    print testEntry,"classified as : ",classifyNB(thisDoc, p0V, p1V, pAb)
+    
+    #测试2
+    testEntry = ['stupid','garbage']
+    thisdoc = array(setOfWords2Vex(myVocabList, testEntry))
+    print testEntry,"classified as : ",classifyNB(thisDoc, p0V, p1V, pAb)
     
     
     
-
-
 
 
---
+++ src/classify/naiveBayes/test.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-    或者  #coding=utf-8
+'''
+Created on 2016年8月20日
+
+@author: xiaoyuan
+'''
+
+
+import bayes
+bayes.testingNB()
